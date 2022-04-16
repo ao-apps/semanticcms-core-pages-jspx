3f9e8e8f9d43d8a76a131d93c7059d31057d6fd1
AO Industries, Inc <support@aoindustries.com>
2022-04-16 02:01:14 -0500

    Added blank line before package declarations
    Resolves "EmptyLineSeparator: 'package' should be separated from previous line."

--- src/main/java/com/semanticcms/core/pages/jspx/JspxPageRepository.java
+++ src/main/java/com/semanticcms/core/pages/jspx/JspxPageRepository.java
@@ -1,6 +1,6 @@
 /*
  * semanticcms-core-pages-jspx - SemanticCMS pages produced by JSPX in the local servlet container.
- * Copyright (C) 2017, 2018, 2019, 2020, 2021  AO Industries, Inc.
+ * Copyright (C) 2017, 2018, 2019, 2020, 2021, 2022  AO Industries, Inc.
  *     support@aoindustries.com
  *     7262 Bull Pen Cir
  *     Mobile, AL 36695
@@ -20,6 +20,7 @@
  * You should have received a copy of the GNU Lesser General Public License
  * along with semanticcms-core-pages-jspx.  If not, see <https://www.gnu.org/licenses/>.
  */
+
 package com.semanticcms.core.pages.jspx;
 
 import com.aoapps.hodgepodge.util.Tuple2;
